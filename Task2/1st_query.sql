cf5c817082b4437886e210f95b19ed4add0855e9
Korent <maj.korent@sers.si>
2024-09-03 21:56:50 +0200

    calculating total trading volume per day

--- Task2/1st_query.sql
+++ Task2/1st_query.sql
@@ -1,31 +1,8 @@
-WITH VolumeStats AS (
-    SELECT 
-        unique_id,
-        SUM(daily_trading_volume) AS total_volume,
-        AVG(daily_trading_volume) AS average_volume,
-        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY daily_trading_volume) AS median_volume
-    FROM 
-        Task_2_trading_volume
-    GROUP BY 
-        unique_id
-),
-ExchangeAvg AS (
-    SELECT 
-        AVG(SUM(daily_trading_volume)) OVER () AS exchange_avg_volume
-    FROM 
-        Task_2_trading_volume
-    GROUP BY 
-        unique_id
-)
 SELECT 
-    V.unique_id,
-    V.total_volume,
-    V.average_volume,
-    V.median_volume,
-    CASE 
-        WHEN V.total_volume > E.exchange_avg_volume THEN 'True'
-        ELSE 'False'
-    END AS volume_above_average
+    unique_id,
+    total_volume,
+    average_volume,
+    median_volume,
+    is_volume_above_average
 FROM 
-    VolumeStats V, 
-    ExchangeAvg E;
+    Trading_Volume_Summary;
