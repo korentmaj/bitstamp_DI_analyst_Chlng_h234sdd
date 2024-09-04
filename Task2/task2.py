39bad5ee2a55a14a088190028c31211205f909c6
Korent <maj.korent@sers.si>
2024-09-04 21:19:39 +0200

    data folder

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-userprofile_df = pd.read_csv('Task2/task2_userprofile.csv')
-trading_volume_df = pd.read_csv('Task2/task2_trading_volume.csv')
-entities_mapping_df = pd.read_csv('Task2/task2_entities_mapping.csv')
+userprofile_df = pd.read_csv('Task2/Data/task2_userprofile.csv')
+trading_volume_df = pd.read_csv('Task2/Data/task2_trading_volume.csv')
+entities_mapping_df = pd.read_csv('Task2/Data/task2_entities_mapping.csv')
 
 
 userprofile_info = userprofile_df.info(), userprofile_df.head()
